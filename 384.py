2cc97ce05acd9f2a0694263b3cd57fc1300a57e5
reena47124 <reenarathode47@gmail.com>
2026-04-23 21:32:37 +0530

    update

--- 384.py
+++ 384.py
@@ -1,8 +1,7 @@
 #set
-#find the difference between two sets.
+#find the symmetric difference between two sets.
 #method 1)
 s1={1,2,3,4,5,'a',8}
 s2={'a',3,'b',5,8}
-result=s1-s2
+result=s1^s2
 print(result)
-
