40eff7711e0c1be4680283e39df7491551e3ca41
reena47124 <reenarathode47@gmail.com>
2026-05-13 16:55:22 +0530

    update

---
+++ 464.py
@@ -0,0 +1,6 @@
+#file_handling in python
+#append text  to existing file.
+#method 1)
+file=open("name.txt","a")
+file.write("\n hello there!")
+file.close()
---
+++ 465.py
@@ -0,0 +1,5 @@
+#file_handling in python
+#append text  to existing file.
+#method 2)
+with open("name.txt","a") as file:
+    file.write("\n hello there!")
