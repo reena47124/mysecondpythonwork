0914e762ed7d90ba5078a556e54934d810d99511
reena47124 <reenarathode47@gmail.com>
2026-05-19 16:13:31 +0530

    update

---
+++ 497.py
@@ -0,0 +1,5 @@
+#numpy
+#find data type of an array element
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr.dtype)
---
+++ 498.py
@@ -0,0 +1,6 @@
+#numpy
+#convert integer array into a float array
+import numpy as np
+arr1=np.array([[1,2,3],[4,5,6]])
+arr2=arr1.astype(float)
+print(arr2)
