1995868ada578b91e37db99cba31009eac9b3537
reena47124 <reenarathode47@gmail.com>
2026-05-19 16:29:17 +0530

    update

---
+++ 499.py
@@ -0,0 +1,6 @@
+#numpy
+#access the first and last element of an array
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(f"first element:{arr[0]}")
+print(f"last element:{arr[-1]}")
---
+++ 500.py
@@ -0,0 +1,5 @@
+#numpy
+#extract elements from index 3 to 8
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9,10])
+print(arr[3:9])
