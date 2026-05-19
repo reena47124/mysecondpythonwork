b86bbde82a87947e5e5c156769a40ceda3c8aa84
reena47124 <reenarathode47@gmail.com>
2026-05-19 15:23:36 +0530

    update

---
+++ 486.py
@@ -0,0 +1,8 @@
+#numpy library in python
+#create a numpy array from a python list.
+import numpy as np
+#a=[1,2,3,4,5]
+#arr=np.array(a)
+#print(arr)
+arr=np.array([1,2,3,4,5,6,7])
+print(arr)
---
+++ 487.py
@@ -0,0 +1,8 @@
+#numpy library in python
+#create a 1D array of numbers from 1 to 20.
+import numpy as np
+arr1=np.arange(1,20)
+arr2=np.arange(1,20,3)
+print(arr1)
+print(arr2)
+
