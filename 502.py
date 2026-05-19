7089045e7d5f4fb7e6186b00b46a5fd08b3b7adb
reena47124 <reenarathode47@gmail.com>
2026-05-19 16:42:02 +0530

    update

---
+++ 502.py
@@ -0,0 +1,5 @@
+#numpy
+#extract the 2nd row from a 2D array
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(f"second row is:{arr[1]}")
---
+++ 503.py
@@ -0,0 +1,5 @@
+#numpy
+#extract a 3rd column from a matrix
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(f"3rd column is:{arr[:,2]}")
