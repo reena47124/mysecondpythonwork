04f7ffb232e71fc2a736115214eb95800fba008c
reena47124 <reenarathode47@gmail.com>
2026-06-21 16:18:38 +0530

    update

---
+++ 534.py
@@ -0,0 +1,12 @@
+#move all zeros to the end
+a=[1,0,3,0,5,0,7]
+nums=[]
+zeros=[]
+for i in range(len(a)):
+    if a[i]==0:
+        zeros.append(a[i])
+    else:
+        nums.append(a[i])
+for i in range(len(zeros)):
+    nums.append(zeros[i]) 
+print(nums)               
