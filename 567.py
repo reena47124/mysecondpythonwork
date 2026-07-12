212ab9fca968e0af05a7a20ce2bf7bda5639b5f9
reena <reenarathode47@gmail.com>
2026-07-12 23:03:10 +0530

    update

---
+++ 567.py
@@ -0,0 +1,10 @@
+#largest of three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+largest = a
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+print("Largest =", largest)
